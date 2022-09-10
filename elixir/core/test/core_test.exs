75a5c094a5457b2dcafec497123b3452acc84aa0
Cory Finger <cory.j.finger@gmail.com>
2022-09-10 14:08:21 -0700

    Write basic test

--- elixir/core/test/core_test.exs
+++ elixir/core/test/core_test.exs
@@ -2,7 +2,13 @@ defmodule CoreTest do
   use ExUnit.Case
   doctest Core
 
-  test "greets the world" do
-    assert Core.hello() == :world
+  setup do
+    %{core: Core.new()}
+  end
+
+  test "greets the world", %{core: c} do
+    assert Core.multiply_by_two(c, 25) == 50
+    assert Core.multiply_by_two(c, 50) == 100
+    assert Core.multiply_by_two(c, 125) == 250
   end
 end
