e3dc68f483fd9ffb6928cdda9a3ed581784fa4b7
Cory Finger <cory.j.finger@gmail.com>
2022-09-11 19:57:43 -0700

    Add report_log() noop for JS

--- nodejs/core/src/wasm.js
+++ nodejs/core/src/wasm.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const modData = fs.readFileSync('./vendor/lib_core.wasm');
 const imports = {
-  env: {respond: (a, b) => {}}
+  env: {report_log: (addr, length) => {}}
 };
 
 module.exports.wasmPromise = WebAssembly.instantiate(new Uint8Array(modData), imports)
